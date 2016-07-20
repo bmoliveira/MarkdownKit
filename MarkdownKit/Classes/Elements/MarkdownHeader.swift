9f256fe762eaf2314eb0b911e6f0d8f41f9f1361
Ivan Bruel <ivan.bruel@gmail.com>
2016-07-20 12:31:34 +0100

    Added font increase to headers

--- MarkdownKit/Classes/Elements/MarkdownHeader.swift
+++ MarkdownKit/Classes/Elements/MarkdownHeader.swift
@@ -15,17 +15,20 @@ public class MarkdownHeader: MarkdownLevelElement {
   public var maxLevel: Int
   public var font: UIFont?
   public var color: UIColor?
+  public var fontIncrease: Int
   
   public var regex: String {
     let level = maxLevel > 0 ? "\(maxLevel)" : ""
     return String(format: MarkdownHeader.regex, level)
   }
   
-  public init(maxLevel: Int = 0, font: UIFont? = UIFont.boldSystemFontOfSize(UIFont.smallSystemFontSize()),
-       color: UIColor? = nil) {
+  public init(maxLevel: Int = 0,
+              font: UIFont? = UIFont.boldSystemFontOfSize(UIFont.smallSystemFontSize()),
+              fontIncrease: Int = 2, color: UIColor? = nil) {
     self.maxLevel = maxLevel
     self.font = font
     self.color = color
+    self.fontIncrease = fontIncrease
   }
   
   public func formatText(attributedString: NSMutableAttributedString, range: NSRange, level: Int) {
@@ -35,7 +38,7 @@ public class MarkdownHeader: MarkdownLevelElement {
   public func attributesForLevel(level: Int) -> [String: AnyObject] {
     var attributes = self.attributes
     if let font = font {
-      let headerFontSize: CGFloat = font.pointSize + (CGFloat(level) * 2)
+      let headerFontSize: CGFloat = font.pointSize + (CGFloat(level) * CGFloat(fontIncrease))
       attributes[NSFontAttributeName] = font.fontWithSize(headerFontSize)
     }
     return attributes
