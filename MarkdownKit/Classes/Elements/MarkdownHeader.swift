5036862aadf0799079f87c940226cc6066ebddd7
Yohannes Wijaya <readyforservice@icloud.com>
2016-10-20 22:21:11 +0800

    Fixed parsing of markdown's heading elements

--- MarkdownKit/Classes/Elements/MarkdownHeader.swift
+++ MarkdownKit/Classes/Elements/MarkdownHeader.swift
@@ -38,7 +38,42 @@ public class MarkdownHeader: MarkdownLevelElement {
   public func attributesForLevel(level: Int) -> [String: AnyObject] {
     var attributes = self.attributes
     if let font = font {
-      let headerFontSize: CGFloat = font.pointSize + (CGFloat(level) * CGFloat(fontIncrease))
+      /***
+       Previously when parsed:
+        #foo  => foo of size 20
+        ######foo => foo of size 32
+       
+       Now when parsed (as it should be conforming to markdown syntax):
+       #foo  => foo of size 32
+       ######foo => foo of size 20
+      ***/
+      
+      var actualLevel: CGFloat = 0
+      if case 0 = level {
+        actualLevel = CGFloat(level) + 6
+      }
+      else if case 1 = level {
+        actualLevel = CGFloat(level) + 4
+      }
+      else if case 2 = level {
+        actualLevel = CGFloat(level) + 2
+      }
+      else if case 3 = level {
+        actualLevel = CGFloat(level) + 0
+      }
+      else if case 4 = level {
+        actualLevel = CGFloat(level) - 2
+      }
+      else if case 5 = level {
+        actualLevel = CGFloat(level) - 4
+      }
+      else if case 6 = level {
+        actualLevel = CGFloat(level) - 6
+      }
+      else {
+        actualLevel = 0
+      }
+      let headerFontSize: CGFloat = font.pointSize + (actualLevel * CGFloat(fontIncrease))
       attributes[NSFontAttributeName] = font.withSize(headerFontSize)
     }
     return attributes
