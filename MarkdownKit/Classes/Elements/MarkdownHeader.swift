22dca650c21b067f97b2f0bf6f5f452eef16c0ac
Yohannes Wijaya <readyforservice@icloud.com>
2016-10-21 12:11:11 +0800

    Refactor previous commit: 'Fixed parsing of markdown's heading elements'

--- MarkdownKit/Classes/Elements/MarkdownHeader.swift
+++ MarkdownKit/Classes/Elements/MarkdownHeader.swift
@@ -11,6 +11,16 @@ import UIKit
 public class MarkdownHeader: MarkdownLevelElement {
 
   private static let regex = "^(#{1,%@})\\s*(.+)$"
+  
+  private enum MarkdownHeadingElements: CGFloat {
+    case oneHash = 6,
+    twoHashes = 4,
+    threeHashes = 2,
+    fourHashes = 0,
+    fiveHashes = -2,
+    sixHashes = -4,
+    zeroHash = -6
+  }
 
   public var maxLevel: Int
   public var font: UIFont?
@@ -48,31 +58,32 @@ public class MarkdownHeader: MarkdownLevelElement {
        ######foo => foo of size 20
       ***/
       
-      var actualLevel: CGFloat = 0
+      var actualLevel: CGFloat
       if case 0 = level {
-        actualLevel = CGFloat(level) + 6
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.oneHash.rawValue
       }
       else if case 1 = level {
-        actualLevel = CGFloat(level) + 4
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.twoHashes.rawValue
       }
       else if case 2 = level {
-        actualLevel = CGFloat(level) + 2
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.threeHashes.rawValue
       }
       else if case 3 = level {
-        actualLevel = CGFloat(level) + 0
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.fourHashes.rawValue
       }
       else if case 4 = level {
-        actualLevel = CGFloat(level) - 2
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.fiveHashes.rawValue
       }
       else if case 5 = level {
-        actualLevel = CGFloat(level) - 4
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.sixHashes.rawValue
       }
       else if case 6 = level {
-        actualLevel = CGFloat(level) - 6
+        actualLevel = CGFloat(level) + MarkdownHeadingElements.zeroHash.rawValue
       }
       else {
-        actualLevel = 0
+        actualLevel = MarkdownHeadingElements.fourHashes.rawValue
       }
+      
       let headerFontSize: CGFloat = font.pointSize + (actualLevel * CGFloat(fontIncrease))
       attributes[NSFontAttributeName] = font.withSize(headerFontSize)
     }
