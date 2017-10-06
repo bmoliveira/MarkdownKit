fa3eebdb2e17baa2199fb173bb3b203b34a98b38
thomas-james-1986 <30452349+thomas-james-1986@users.noreply.github.com>
2017-10-06 11:46:18 +0100

    - Grabble fork of MarkdownKit
    - Can be initialised via a dictionary of attributes

---
+++ MarkdownKit/Classes/Elements/MarkdownType.swift
@@ -0,0 +1,19 @@
+//
+//  MarkdownType.swift
+//  Pods
+//
+//  Created by Tom O'Rourke on 5/10/2017
+//
+//
+
+public enum MarkdownType: String {
+    case base
+    case header
+    case list
+    case quote
+    case link
+    case automaticLink
+    case bold
+    case italic
+    case code
+}
