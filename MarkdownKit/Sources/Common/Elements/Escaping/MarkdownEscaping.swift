2ef1290dbfd6268c57e9eca9c77cda5b1f42ea4a
Evgen Bodunov <evgen@getyourmap.com>
2019-09-19 11:58:37 +0300

    Removed redundant public keywords from public extensions. Migrated to Swift 5 Used short array declaration [UInt16] instead of full Array<UInt16>.

--- MarkdownKit/Sources/Common/Elements/Escaping/MarkdownEscaping.swift
+++ MarkdownKit/Sources/Common/Elements/Escaping/MarkdownEscaping.swift
@@ -23,7 +23,7 @@ open class MarkdownEscaping: MarkdownElement {
     let range = NSRange(location: match.range.location + 1, length: 1)
     // escape one character
     let matchString = attributedString.attributedSubstring(from: range).string
-    if let escapedString = Array<UInt16>(matchString.utf16).first
+    if let escapedString = [UInt16](matchString.utf16).first
       .flatMap({ (value: UInt16) -> String in String(format: "%04x", value) }) {
       attributedString.replaceCharacters(in: range, with: escapedString)
     }
