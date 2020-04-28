0e661e5ff4ccd98985cc6ab10ddcdc0e4e1cddb9
Denis Chaschin <denis@chaschin.pro>
2020-04-28 21:21:10 +1000

    Creating unified build instead

--- MarkdownKit/Sources/UIKit/Elements/Header/MarkdownHeader+UIKit.swift
+++ MarkdownKit/Sources/UIKit/Elements/Header/MarkdownHeader+UIKit.swift
@@ -6,8 +6,12 @@
 //  Copyright © 2019 Ivan Bruel. All rights reserved.
 //
 
+#if canImport(UIKit)
+
 import UIKit
 
 public extension MarkdownHeader {
     static let defaultFont = UIFont.boldSystemFont(ofSize: UIFont.smallSystemFontSize)
 }
+
+#endif
