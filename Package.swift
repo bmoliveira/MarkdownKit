3b9a330c9603abdadc26f316b629316f4baa4367
Denis Chaschin <denis@chaschin.pro>
2020-04-28 20:50:15 +1000

    Target name error fixed

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MarkdownKit",
-            targets: ["MarkdownKit"]
+            targets: ["MarkdownKit-Common"]
         ),
         .library(
             name: "MarkdownKit-UIKit",
