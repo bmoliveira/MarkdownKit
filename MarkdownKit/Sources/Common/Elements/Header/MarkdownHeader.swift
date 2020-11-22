1f2453528612f104288aa0ca5dd2fd4b87b3dae3
XC <wbsgxc@gmail.com>
2020-11-22 14:59:44 +0800

    [ACCEDOONECA-3623] Added preset header font support for MDHeader

--- MarkdownKit/Sources/Common/Elements/Header/MarkdownHeader.swift
+++ MarkdownKit/Sources/Common/Elements/Header/MarkdownHeader.swift
@@ -33,12 +33,15 @@ open class MarkdownHeader: MarkdownLevelElement {
       attributedString.deleteCharacters(in: range)
   }
 
-    open func attributesForLevel(_ level: Int) -> [NSAttributedString.Key: AnyObject] {
+  // Modified by Xiaochen Guo (xiaochen.guo@accedo.tv)
+  // The `bold()` function might return nil as some libs have different naming convention
+  // Now allow preset header font
+  open func attributesForLevel(_ level: Int) -> [NSAttributedString.Key: AnyObject] {
     var attributes = self.attributes
     if let font = font {
         let headerFontSize: CGFloat = font.pointSize + 4 + (-1 * CGFloat(level) * CGFloat(fontIncrease))
-      
-      attributes[NSAttributedString.Key.font] = font.withSize(headerFontSize).bold()
+        let targetFont = font.withSize((headerFontSize > font.pointSize) ? headerFontSize : font.pointSize)
+        attributes[NSAttributedString.Key.font] = targetFont.bold() ?? targetFont
     }
     return attributes
   }
