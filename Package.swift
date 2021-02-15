ffd7a63d2df2bc96d2bc57015b7f8e5af02944dc
Andreas Verhoeven <ave@aveapps.com>
2021-02-15 19:30:01 +0100

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+	name: "MarkdownKit",
+	platforms: [.macOS(.v10_13), .iOS(.v10), .tvOS(.v13)],
+	products: [
+		.library(
+			name: "MarkdownKit",
+			targets: ["MarkdownKit"]
+		)
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "MarkdownKit",
+			dependencies: []),
+	],
+	cxxLanguageStandard: .cxx14
+)
