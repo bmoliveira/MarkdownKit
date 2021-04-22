8efefcf613df18e1e8769ea9fef465f4f26294c0
Jan Gaebel <g.jan94@icloud.com>
2021-04-22 22:53:48 +0200

    Revert "Adds bold+italic and replacing default elements"
    This reverts commit 55339cd822ad7f330c26c0f7f2a0377fb1b5e53b.

--- MarkdownKit/Sources/Common/Elements/Italic/MarkdownItalic.swift
+++ MarkdownKit/Sources/Common/Elements/Italic/MarkdownItalic.swift
@@ -22,24 +22,4 @@ open class MarkdownItalic: MarkdownCommonElement {
     self.font = font?.italic()
     self.color = color
   }
-
-  public func match(_ match: NSTextCheckingResult, attributedString: NSMutableAttributedString) {
-    // deleting trailing markdown
-    attributedString.deleteCharacters(in: match.range(at: 4))
-
-    // formatting string (may alter the length)
-    let stringAttributes = attributedString.attributes(at: match.range(at: 3).location, longestEffectiveRange: nil, in: match.range(at: 3))
-    addAttributes(attributedString, range: match.range(at: 3))
-    if let font = font,
-      let stringFont = stringAttributes[.font] as? MarkdownFont {
-      let fontSize: CGFloat = stringFont.fontDescriptor.pointSize
-      var italicFont: MarkdownFont = font.withSize(fontSize)
-      let fontTraits = stringFont.fontDescriptor.symbolicTraits
-      italicFont = italicFont.withTraits(fontTraits, .traitItalic) ?? italicFont
-      attributedString.addAttributes([NSAttributedString.Key.font: italicFont], range: match.range(at: 3))
-    }
-
-	// deleting leading markdown
-    attributedString.deleteCharacters(in: match.range(at: 2))
-  }
 }
