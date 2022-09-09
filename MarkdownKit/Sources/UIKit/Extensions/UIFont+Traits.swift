ededeee1eedf9661a9897cfb8a35dd4dc8e340d1
Connor Ricks <connor.ricks@gmail.com>
2022-09-09 10:13:46 -0400

    Set pointSize explicitly in order to avoid being overwritten.

--- MarkdownKit/Sources/UIKit/Extensions/UIFont+Traits.swift
+++ MarkdownKit/Sources/UIKit/Extensions/UIFont+Traits.swift
@@ -13,10 +13,11 @@ import UIKit
 extension UIFont {
 
   func withTraits(_ traits: UIFontDescriptor.SymbolicTraits...) -> UIFont? {
+    let pointSize = fontDescriptor.pointSize
     guard let descriptor = fontDescriptor.withSymbolicTraits(UIFontDescriptor.SymbolicTraits(traits)) else {
         return nil
     }
-    return UIFont(descriptor: descriptor, size: 0)
+    return UIFont(descriptor: descriptor, size: pointSize)
   }
 
   func bold() -> UIFont {
