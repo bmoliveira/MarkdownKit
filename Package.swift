8968b5484236b156ec5f87a935ec5d9f76220509
Jared Kanter <jkanter@teladoc.com>
2024-07-02 15:06:10 -0400

    Only do Dynamic

--- Package.swift
+++ Package.swift
@@ -13,11 +13,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MarkdownKit",
-            targets: ["MarkdownKit"]
-        ),
-        .library(
-            name: "MarkdownKit-Dynamic",
-            type: .dynamic,
+            type: .dynamic
             targets: ["MarkdownKit"]
         )
     ],
