bb783b79d9ecda9448884dddae5dc1224a8f9b2e
Bruno Oliveira <bruno.oliveira@remote.com>
2025-06-25 09:21:54 +0100

    bump version to 1.7.3

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MarkdownKit",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v9),
+        .macOS(.v12_4),
+        .iOS(.v15_6),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
